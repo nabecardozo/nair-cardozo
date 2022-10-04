1089209d9542077b9f851fc01d057f062f984cc6
nair cardozo <nair_belenc@hotmailcom>
2022-10-04 09:40:00 -0300

    js-entrega

---
+++ js/codigo.js
@@ -0,0 +1,69 @@
+//declaracion de variable
+
+let nombre;
+let edad;
+
+//inicializacion de esa variable
+nombre = "nair cardozo";
+edad = 23;
+
+//actualizo el valor de la variable
+nombre = "marina";
+edad = 40;
+
+//declarar y asignar al mismo tiempo
+let direccion = "avenida brasil 183"
+
+//constamte
+const PI = 3.14;
+
+//operaciones matematicas basicas
+let num1 =8;
+let num2 =3;
+let suma = num1 + num2; //11
+let resta = num1 - num2; //5
+let multiplicacion = num1 * num2; //24
+let division = num1 / num2;//2.6666
+
+//concatenacion
+let apellido = "gomez";
+let nombreCompleto = nombre +" "+ apellido ; //marina gomez
+
+let nomYEdad = nombre +" "+ edad;//marina40
+
+//consola
+console.log("bienvenidos a la consola");
+console.log("el resultado de la suma es: "+suma);
+console.log("el resultado de la resta es: "+resta);
+console.log("el resultado de la multiplicacion es: "+multiplicacion);
+console.log("el resultado de la division es: "+division);
+
+console.log("El nombre completo del usuario es: "+nombreCompleto);
+console.log("El nombre concatenado a la edad es: "+nomYEdad);
+
+
+//prompt
+
+let bandaFavorita = prompt ("ingresa tu banda favorita");
+console.log("la banda favorita del usuario es: "+bandaFavorita);
+
+let nomUsu = prompt("ingresa tu nombre");
+alert("Buenas noches "+nomUsu);
+
+//ejercicio sumamos dos numeros que ingresa en usuario
+let primerNum = parseInt(prompt("ingresa el primer numero a sumar"));
+let segundoNum = parseInt(prompt("ingresa el segundo numero a sumar"));
+//ya tenemos ambos numeros en formato numerico
+let sumaNumeros = primerNum + segundoNum ;
+alert("la suma de los numeros ingresados es ="+sumaNumeros);
+
+//parseFloat para convertir a numeros decimales por ej un precio.
+
+
+//control de flujos "if"
+
+
+
+
+
+
