850f0c4426b627f05c137caef2d3700bee8dcbca
nair cardozo <nair_belenc@hotmailcom>
2022-10-07 18:22:38 -0300

    corregir

--- js/codigo.js
+++ js/codigo.js
@@ -1,21 +1,4 @@
-//declaracion de variable
-
-let nombre;
-let edad;
-
-//inicializacion de esa variable
-nombre = "nair cardozo";
-edad = 23;
-
-//actualizo el valor de la variable
-nombre = "marina";
-edad = 40;
-
-//declarar y asignar al mismo tiempo
-let direccion = "avenida brasil 183"
-
-//constamte
-const PI = 3.14;
+/*
 
 //operaciones matematicas basicas
 let num1 =8;
@@ -60,9 +43,52 @@ alert("la suma de los numeros ingresados es ="+sumaNumeros);
 //parseFloat para convertir a numeros decimales por ej un precio.
 
 
-//control de flujos "if"
+
+*/
+
+for (let i=1; i<=3; i++){
+    let usuario=prompt("ingresa usuario");
+    let contrasenia=prompt("ingresa contraseña para "+usuario);
+
+    if ((usuario=="naircardozo")&&(contrasenia=="1234")){
+        console.log("bienvenido a la farmacia!!");
+        break;
+    }else{
+        console.log("usuario y/o contraseña incorrecta!! restan "+(3-i)+"intentos");
+    }
+}
 
 
+let fruta = prompt("ingresa la medicación y te dire el precio por cajita(salir para finalizar)");
+
+while (fruta !="salir"){
+    switch(fruta){
+        case "magnesio":
+            console.log("Precio de curflex x 30comprimidos $3020");
+            alert("ya se añadio al carrito!")
+            break;
+        case "diclofenac":
+            console.log("Precio de anaflex $500");
+            alert("ya se añadio al carrito!")
+            break;
+        case "ibuprofeno":
+            console.log("Precio de ibupirac 600mg x 20comprimidos $1300");
+            alert("ya se añadio al carrito!")
+            break;
+        case "paracetamol":
+        case "Paracetamol":
+            console.log("Precio tafirol por 30comprimidos $750");
+            alert("ya se añadio al carrito!")
+            break;
+        default:
+            console.log("medicación sin stock");
+            alert("Disculpas, no tenemos mas stock")
+            break;
+    }
+    fruta=prompt("precisa alguna otra medicación ??? te dire el precio por caja x30compr( escriba {salir} para finalizar)");
+}
+alert("gracias por su compra")
+console.log("finalizo compra!");
 
 
 
