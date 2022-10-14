67fadb2b8558d035690a4cd5455d924d8d7dc3bb
nair cardozo <nair_belenc@hotmailcom>
2022-10-14 17:11:29 -0300

    entregar

--- js/codigo.js
+++ js/codigo.js
@@ -1,51 +1,4 @@
-/*
-
-//operaciones matematicas basicas
-let num1 =8;
-let num2 =3;
-let suma = num1 + num2; //11
-let resta = num1 - num2; //5
-let multiplicacion = num1 * num2; //24
-let division = num1 / num2;//2.6666
-
-//concatenacion
-let apellido = "gomez";
-let nombreCompleto = nombre +" "+ apellido ; //marina gomez
-
-let nomYEdad = nombre +" "+ edad;//marina40
-
-//consola
-console.log("bienvenidos a la consola");
-console.log("el resultado de la suma es: "+suma);
-console.log("el resultado de la resta es: "+resta);
-console.log("el resultado de la multiplicacion es: "+multiplicacion);
-console.log("el resultado de la division es: "+division);
-
-console.log("El nombre completo del usuario es: "+nombreCompleto);
-console.log("El nombre concatenado a la edad es: "+nomYEdad);
-
-
-//prompt
-
-let bandaFavorita = prompt ("ingresa tu banda favorita");
-console.log("la banda favorita del usuario es: "+bandaFavorita);
-
-let nomUsu = prompt("ingresa tu nombre");
-alert("Buenas noches "+nomUsu);
-
-//ejercicio sumamos dos numeros que ingresa en usuario
-let primerNum = parseInt(prompt("ingresa el primer numero a sumar"));
-let segundoNum = parseInt(prompt("ingresa el segundo numero a sumar"));
-//ya tenemos ambos numeros en formato numerico
-let sumaNumeros = primerNum + segundoNum ;
-alert("la suma de los numeros ingresados es ="+sumaNumeros);
-
-//parseFloat para convertir a numeros decimales por ej un precio.
-
-
-
-*/
-
+//ingreso usuario
 for (let i=1; i<=3; i++){
     let usuario=prompt("ingresa usuario");
     let contrasenia=prompt("ingresa contraseña para "+usuario);
@@ -89,6 +42,26 @@ while (fruta !="salir"){
 }
 alert("gracias por su compra")
 console.log("finalizo compra!");
+
+//funcion con parametro y RETURN
+
+let precioMedicina=parseFloat(prompt("ingresa el precio del medicamento"));
+
+
+function calcularIva(precio){
+return precio * 0.21;
+}
+
+//precio=100 -> iva=21
+
+let iva = calcularIva(precioMedicina);
+console.log("El valor del IVA es $"+ iva);
+
+function mostrarPrecioFinal(precioIngresado,ivaCalculado){
+    console.log("El precio con iva incluido es $"+(precioIngresado+ivaCalculado));
+}
+
+mostrarPrecioFinal(precioMedicina,iva);
 
 
 
